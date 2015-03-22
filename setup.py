4062d19b532c259881b8ce4ffb4eeb16cb32d1ea
Ben Doan <ben@bendoan.me>
2015-03-22 00:58:31 -0500

    improved login page styling

--- setup.py
+++ setup.py
@@ -3,3 +3,4 @@ from server import db, create_user, model
 db.create_all()
 
 create_user('admin', 'admin@example.com', 'password', is_admin=True)
+create_user('user', 'user@example.com', 'password', is_admin=False)
